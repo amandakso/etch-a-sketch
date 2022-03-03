3ef42dff07f3cb4ffa8d1f38c9d275e67ef0b18a
Amanda So <soamanda95@gmail.com>
2022-03-02 16:22:42 -0800

    add 16x16 squares to container

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const square=document.createElement("div");
 const container=document.querySelector(".container");
-container.appendChild(square);
+
+let numberOfSquares= 0;
+function generateSquares() {
+    for (numberOfSquares=0; numberOfSquares <(16 * 16); numberOfSquares++){
+        square=document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+};
+generateSquares();
