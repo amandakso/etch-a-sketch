22322f53881c2dd2624f9a82ed4261b98d82ada2
Amanda So <soamanda95@gmail.com>
2022-03-02 18:45:59 -0800

    add color change when mouse goes over square

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const container=document.querySelector(".container");
 
 let numberOfSquares= 0;
 function generateSquares() {
-    for (numberOfSquares=0; numberOfSquares <(16 * 16); numberOfSquares++){
+    for (numberOfSquares=0 * 0; numberOfSquares <(16 * 16); numberOfSquares++){
         square=document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+        
     }
 };
 generateSquares();
+
+const color=document.querySelectorAll("div.square");
+color.forEach((square) => {
+    square.addEventListener("mouseleave", () => {
+        square.style.background=`rgb(${204},${153},${255})`;
+    });
+});
