4b2203edb72f424ae0e29296e7e111d398a9689d
Amanda So <soamanda95@gmail.com>
2022-03-03 14:11:35 -0800

    clear grid after hitting start over button

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const container=document.querySelector(".container");
 const button=document.querySelector(".button");
 
-let numberOfSquares= 0;
 function generateSquares() {
-    for (numberOfSquares=0; numberOfSquares <(16 * 16); numberOfSquares++){
+    for (i=0; i<(16 * 16); i++){
         square=document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
@@ -12,33 +11,52 @@ function generateSquares() {
 };
 generateSquares();
 
+
+const squares=document.querySelectorAll("div.square");
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.background=`rgb(${255},${204},${229})`;
+    });
+});
+button.addEventListener("click", () => {
+    const grid=document.getElementById("grid");
+    while(grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    };
+    promptQuestion();
+
+});
+//prompt question is not retaining num
+function promptQuestion() {
+    let num=window.prompt("How many squares per side?");
+    while (Number.isSafeInteger(parseFloat(num))==false|| parseFloat(num)<1 || parseFloat(num)>100)
+    if (Number.isSafeInteger(parseFloat(num))==true && parseFloat(num)>=1 && parseFloat(num)<= 100) {
+        console.log
+    } else {
+        num=window.prompt("Invalid answer. How many squares per side? Pick number between 1 & 100");
+    };
+};
+
+/*
+button.addEventListener("click", () => {
+    let num=window.prompt("How many squares per side?");
+    while (Number.isSafeInteger(parseFloat(num))==false|| parseFloat(num)<1 || parseFloat(num)>100)
+    if (Number.isSafeInteger(parseFloat(num))==true && parseFloat(num)>=1 && parseFloat(num)<= 100) {
+        console.log(num);
+    } else {
+        num=window.prompt("Invalid answer. How many squares per side? Pick number between 1 & 100");
+    };
+});
+*/
+
 function resetSquares(val) {
-    let numberOfSquares=0;
     let col=100/parseFloat(val);
     let change=document.getElementById(".container");
     change.style.gridTemplateColumns=`repeat(parseFloat(${val}),${col}%)`;
     change.style.gridTemplateRows=`repeat(parseFloat(${val}),${col}%)`;
-    for(numberOfSquares=0; numberOfSquares <(val * val); numberOfSquares++) {
+    for(i=0; i<(val * val); i++) {
         square=document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
     };
 };
-
-const color=document.querySelectorAll("div.square");
-color.forEach((square) => {
-    square.addEventListener("mouseleave", () => {
-        square.style.background=`rgb(${255},${204},${229})`;
-    });
-});
-
-button.addEventListener("click", () => {
-    let num=window.prompt("How many squares per side?");
-    while (Number.isSafeInteger(parseFloat(num))==false|| parseFloat(num)<1 || parseFloat(num)>100)
-    if (Number.isSafeInteger(parseFloat(num))==true && parseFloat(num)>=1 && parseFloat(num)<= 100) {
-    color.container.removeChild(square);
-    resetSquares(num);
-    } else {
-        num=window.prompt("Invalid answer. How many squares per side? Pick number between 1 & 100");
-    };
-});
