9966109b24a2c7531d2b25628a4885535f97f141
Amanda So <soamanda95@gmail.com>
2022-03-08 15:44:51 -0800

    add function for painting squares different colors

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container=document.querySelector(".container");
 const button=document.querySelector(".button");
 const startover=document.querySelector(".startover");
-
+const black=document.querySelector(".black");
+const blue=document.querySelector(".blue");
+const pastel=document.querySelector(".pastel");
+const rainbow=docmument.querySelector(".rainbow");
+const gray=document.querySelector(".gray");
+const eraser=document.querySelector(".eraser");
+const ink=document.querySelector(".color");
 
 function generateSquares(num) {
     let x=parseFloat(num);
@@ -9,20 +15,35 @@ function generateSquares(num) {
         const square=document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);  
-        square.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor='black';
-            let opacity= Number(event.target.style.opacity);
-            if (opacity< 1) {
-                opacity=opacity + 0.1;
-            }
-            event.target.style.opacity= opacity;
+        square.addEventListener("mouseover", paintSquare());
         });
         };
     };
 
+ /*   square.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor='black';
+        let opacity= Number(event.target.style.opacity);
+        if (opacity< 1) {
+            opacity=opacity + 0.1;
+        }
+        event.target.style.opacity= opacity;
+ */
+
 generateSquares(16);
 
-function pickColor() {
+function paintSquare(event) {
+        switch (selectColor()) {
+            case black:
+                event.target.style.backgroundColor="black";
+                break;
+            case blue:
+                event.target.style.backgroundColor="blue";
+            default:
+                event.target.style.backgroundColor="black";
+        };
+
+};
+function selectColor() {
     
 }
 
